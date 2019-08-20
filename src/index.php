9d3f29cb4f7dcb0a1ba38475f2a568e735288758
dmavridis <dmavridis@gmail.com>
2019-08-20 20:30:35 +0100

    Initial import

---
+++ src/index.php
@@ -0,0 +1,7 @@
+<?php
+
+echo "Hello, World from Kubernetes! <br>";
+echo "Hello Jenkins X! <br>";
+echo '<img border="0" src="https://jenkins-x.io/images/logo.png" width="300" height="300" />';
+
+?>
